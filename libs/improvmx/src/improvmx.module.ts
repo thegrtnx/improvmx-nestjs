4a3a1c3582ba949d8c1e7decf794ca001855c43b
Greatness Abolade <49532326+thegrtnx@users.noreply.github.com>
2024-06-26 00:43:08 +0100

    👌Update: APIs Endpoint

--- libs/improvmx/src/improvmx.module.ts
+++ libs/improvmx/src/improvmx.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 import { ImprovmxService } from './improvmx.service';
 
 @Module({
+  imports: [ConfigModule, HttpModule],
   providers: [ImprovmxService],
   exports: [ImprovmxService],
 })
