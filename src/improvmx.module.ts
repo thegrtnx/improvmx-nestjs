f653151e3637afabbabbd8db0d47e6daedb95bac
Greatness Abolade <49532326+thegrtnx@users.noreply.github.com>
2024-06-26 05:33:02 +0100

    🐛 Bug: Global module

--- src/improvmx.module.ts
+++ src/improvmx.module.ts
@@ -1,8 +1,9 @@
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 import { HttpModule } from "@nestjs/axios";
 import { ConfigModule } from "@nestjs/config";
 import { ImprovmxService } from "./improvmx.service";
 
+@Global()
 @Module({
 	imports: [HttpModule, ConfigModule],
 	providers: [ImprovmxService],
